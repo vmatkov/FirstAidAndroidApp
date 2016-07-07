b0244c9601f4e87501c4a2ed8f40d9cfed4c8d2e
Matjaz Voros <vmatkov@gmail.com>
2016-07-07 15:54:33 +0200

    First commit

---
+++ PrvaPomoc/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PrvaPomoc
+{
+    [Activity(Label = "Prva pomoč")]
+    public class MainActivity : Activity
+    {
+        //int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            //Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            //var metrics = Resources.DisplayMetrics;
+            //var widthInPx = metrics.WidthPixels;
+            //var heightInPx = metrics.HeightPixels;
+
+            //FindViewById<TextView>(Resource.Id.screenWidthDp).Text = "Screen Width: " + widthInPx + " px.";
+            //FindViewById<TextView>(Resource.Id.screenHeightDp).Text = "Screen Height: " + heightInPx + " px.";
+        }
+    }
+}
+

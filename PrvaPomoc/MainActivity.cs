9fc3b05491d5f68ec2a6852d49847e0881c34b1f
Matjaz Voros <vmatkov@gmail.com>
2016-07-10 15:49:08 +0200

    Revert "MenuItemPictures"
    This reverts commit da607f30f60d6f59a115e6463b8ba487fc594478.

--- PrvaPomoc/MainActivity.cs
+++ PrvaPomoc/MainActivity.cs
@@ -7,15 +7,14 @@ using Android.Widget;
 using Android.OS;
 using System.Collections.Generic;
 using Android.Media;
-using Android.Graphics;
 
 namespace PrvaPomoc
 {
     [Activity(Label = "Prva pomoč")]
     public class MainActivity : Activity
     {
-        private List<Image> clickableItems;
-        //private List<string> clickableItems;
+        //private List<Image> clickableItems;
+        private List<string> clickableItems;
         private ListView mListView;
 
         protected override void OnCreate(Bundle bundle)
@@ -30,21 +29,15 @@ namespace PrvaPomoc
 
             //ArrayAdapter<Image> adapter = new ArrayAdapter<Image>(this, Android.Resource.Layout.SimpleListItem1, clickableItems);
 
-            //clickableItems = new List<string>();
-            //clickableItems.Add("PRVA POMOČ");
-            //clickableItems.Add("Koliko vem o nudenju prve pomoči?");
-            //clickableItems.Add("Zemljevid AED naprav");
-            //clickableItems.Add("Pokliči 112");
+            clickableItems = new List<string>();
+            clickableItems.Add("PRVA POMOČ");
+            clickableItems.Add("Koliko vem o nudenju prve pomoči?");
+            clickableItems.Add("Zemljevid AED naprav");
+            clickableItems.Add("Pokliči 112");
 
-            //clickableItems = new List<Image>();
-            //clickableItems.Add(new Bitmap());
-            //clickableItems.Add("Koliko vem o nudenju prve pomoči?");
-            //clickableItems.Add("Zemljevid AED naprav");
-            //clickableItems.Add("Pokliči 112");
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, clickableItems);
 
-            //ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, clickableItems);
-
-            //mListView.Adapter = adapter;
+            mListView.Adapter = adapter;
 
         }
     }

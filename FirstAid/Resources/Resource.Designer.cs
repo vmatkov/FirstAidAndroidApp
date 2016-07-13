3a9bf3713a73d569d665d981c57053cb29bffca0
Matjaz Voros <vmatkov@gmail.com>
2016-07-13 12:26:13 +0200

    Spremenjen design

--- FirstAid/Resources/Resource.Designer.cs
+++ FirstAid/Resources/Resource.Designer.cs
@@ -31,39 +31,6 @@ namespace FirstAid
 		public partial class Attribute
 		{
 			
-			// aapt resource value: 0x7f010000
-			public const int cardBackgroundColor = 2130771968;
-			
-			// aapt resource value: 0x7f010001
-			public const int cardCornerRadius = 2130771969;
-			
-			// aapt resource value: 0x7f010002
-			public const int cardElevation = 2130771970;
-			
-			// aapt resource value: 0x7f010003
-			public const int cardMaxElevation = 2130771971;
-			
-			// aapt resource value: 0x7f010005
-			public const int cardPreventCornerOverlap = 2130771973;
-			
-			// aapt resource value: 0x7f010004
-			public const int cardUseCompatPadding = 2130771972;
-			
-			// aapt resource value: 0x7f010006
-			public const int contentPadding = 2130771974;
-			
-			// aapt resource value: 0x7f01000a
-			public const int contentPaddingBottom = 2130771978;
-			
-			// aapt resource value: 0x7f010007
-			public const int contentPaddingLeft = 2130771975;
-			
-			// aapt resource value: 0x7f010008
-			public const int contentPaddingRight = 2130771976;
-			
-			// aapt resource value: 0x7f010009
-			public const int contentPaddingTop = 2130771977;
-			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -77,29 +44,17 @@ namespace FirstAid
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int cardview_dark_background = 2131034112;
+			// aapt resource value: 0x7f050002
+			public const int theme_custom_background = 2131034114;
 			
 			// aapt resource value: 0x7f050001
-			public const int cardview_light_background = 2131034113;
+			public const int theme_devicedefault_background = 2131034113;
 			
-			// aapt resource value: 0x7f050002
-			public const int cardview_shadow_end_color = 2131034114;
+			// aapt resource value: 0x7f050000
+			public const int theme_firstaid_background = 2131034112;
 			
 			// aapt resource value: 0x7f050003
-			public const int cardview_shadow_start_color = 2131034115;
-			
-			// aapt resource value: 0x7f050006
-			public const int theme_custom_background = 2131034118;
-			
-			// aapt resource value: 0x7f050005
-			public const int theme_devicedefault_background = 2131034117;
-			
-			// aapt resource value: 0x7f050004
-			public const int theme_firstaid_background = 2131034116;
-			
-			// aapt resource value: 0x7f050007
-			public const int theme_material_settings_background = 2131034119;
+			public const int theme_material_settings_background = 2131034115;
 			
 			static Color()
 			{
@@ -107,28 +62,6 @@ namespace FirstAid
 			}
 			
 			private Color()
-			{
-			}
-		}
-		
-		public partial class Dimension
-		{
-			
-			// aapt resource value: 0x7f060000
-			public const int cardview_compat_inset_shadow = 2131099648;
-			
-			// aapt resource value: 0x7f060001
-			public const int cardview_default_elevation = 2131099649;
-			
-			// aapt resource value: 0x7f060002
-			public const int cardview_default_radius = 2131099650;
-			
-			static Dimension()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Dimension()
 			{
 			}
 		}
@@ -185,17 +118,17 @@ namespace FirstAid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f080000
-			public const int MaterialItem1 = 2131230720;
+			// aapt resource value: 0x7f070000
+			public const int MaterialItem1 = 2131165184;
 			
-			// aapt resource value: 0x7f080001
-			public const int MaterialItem2 = 2131230721;
+			// aapt resource value: 0x7f070001
+			public const int MaterialItem2 = 2131165185;
 			
-			// aapt resource value: 0x7f080002
-			public const int MaterialItem3 = 2131230722;
+			// aapt resource value: 0x7f070002
+			public const int MaterialItem3 = 2131165186;
 			
-			// aapt resource value: 0x7f080003
-			public const int MaterialItem4 = 2131230723;
+			// aapt resource value: 0x7f070003
+			public const int MaterialItem4 = 2131165187;
 			
 			static Id()
 			{
@@ -226,11 +159,11 @@ namespace FirstAid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f070001
-			public const int ApplicationName = 2131165185;
+			// aapt resource value: 0x7f040001
+			public const int ApplicationName = 2130968577;
 			
-			// aapt resource value: 0x7f070000
-			public const int Hello = 2131165184;
+			// aapt resource value: 0x7f040000
+			public const int Hello = 2130968576;
 			
 			static String()
 			{
@@ -245,29 +178,17 @@ namespace FirstAid
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int Base_CardView = 2130968577;
+			// aapt resource value: 0x7f060002
+			public const int Theme_Custom = 2131099650;
 			
-			// aapt resource value: 0x7f040000
-			public const int CardView = 2130968576;
+			// aapt resource value: 0x7f060003
+			public const int Theme_Custom1 = 2131099651;
 			
-			// aapt resource value: 0x7f040002
-			public const int CardView_Dark = 2130968578;
+			// aapt resource value: 0x7f060001
+			public const int Theme_FirstAid = 2131099649;
 			
-			// aapt resource value: 0x7f040003
-			public const int CardView_Light = 2130968579;
-			
-			// aapt resource value: 0x7f040006
-			public const int Theme_Custom = 2130968582;
-			
-			// aapt resource value: 0x7f040007
-			public const int Theme_Custom1 = 2130968583;
-			
-			// aapt resource value: 0x7f040005
-			public const int Theme_FirstAid = 2130968581;
-			
-			// aapt resource value: 0x7f040004
-			public const int Theme_Splash = 2130968580;
+			// aapt resource value: 0x7f060000
+			public const int Theme_Splash = 2131099648;
 			
 			static Style()
 			{
@@ -275,73 +196,6 @@ namespace FirstAid
 			}
 			
 			private Style()
-			{
-			}
-		}
-		
-		public partial class Styleable
-		{
-			
-			public static int[] CardView = new int[] {
-					16843071,
-					16843072,
-					2130771968,
-					2130771969,
-					2130771970,
-					2130771971,
-					2130771972,
-					2130771973,
-					2130771974,
-					2130771975,
-					2130771976,
-					2130771977,
-					2130771978};
-			
-			// aapt resource value: 1
-			public const int CardView_android_minHeight = 1;
-			
-			// aapt resource value: 0
-			public const int CardView_android_minWidth = 0;
-			
-			// aapt resource value: 2
-			public const int CardView_cardBackgroundColor = 2;
-			
-			// aapt resource value: 3
-			public const int CardView_cardCornerRadius = 3;
-			
-			// aapt resource value: 4
-			public const int CardView_cardElevation = 4;
-			
-			// aapt resource value: 5
-			public const int CardView_cardMaxElevation = 5;
-			
-			// aapt resource value: 7
-			public const int CardView_cardPreventCornerOverlap = 7;
-			
-			// aapt resource value: 6
-			public const int CardView_cardUseCompatPadding = 6;
-			
-			// aapt resource value: 8
-			public const int CardView_contentPadding = 8;
-			
-			// aapt resource value: 12
-			public const int CardView_contentPaddingBottom = 12;
-			
-			// aapt resource value: 9
-			public const int CardView_contentPaddingLeft = 9;
-			
-			// aapt resource value: 10
-			public const int CardView_contentPaddingRight = 10;
-			
-			// aapt resource value: 11
-			public const int CardView_contentPaddingTop = 11;
-			
-			static Styleable()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Styleable()
 			{
 			}
 		}

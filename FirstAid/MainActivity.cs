caae3f1ef774a1f93bd5685d38d99466740beab1
Matjaz Voros <vmatkov@gmail.com>
2016-07-13 13:14:52 +0200

    Funkcije dodane na iteme

--- FirstAid/MainActivity.cs
+++ FirstAid/MainActivity.cs
@@ -19,6 +19,36 @@ namespace FirstAid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            var Item1 = FindViewById<ImageView>(Resource.Id.MaterialItem1);
+            var Item2 = FindViewById<ImageView>(Resource.Id.MaterialItem2);
+            var Item3 = FindViewById<ImageView>(Resource.Id.MaterialItem2);
+            var Item4 = FindViewById<ImageView>(Resource.Id.MaterialItem3);
+
+            Item1.Click += Item1_Click;
+            Item2.Click += Item2_Click;
+            Item3.Click += Item3_Click;
+            Item4.Click += Item4_Click;
+
+        }
+
+        private void Item1_Click(object sender, EventArgs e)
+        {
+            // Nujna medicinska pomoč
+        }
+
+        private void Item2_Click(object sender, EventArgs e)
+        {
+            // Koliko vem o nudenju prve pomoči?
+        }
+
+        private void Item3_Click(object sender, EventArgs e)
+        {
+            // Zemljevid AED naprav po Sloveniji
+        }
+
+        private void Item4_Click(object sender, EventArgs e)
+        {
+            // Klic 112
         }
     }
 }
